37c215531ed8c92e0faf8bf889461f87c7d6ecee
yesseruser <yesseruseryt@gmail.com>
2023-02-21 20:16:46 +0100

    Added a CloseApp method.

--- yTools/General.cs
+++ yTools/General.cs
@@ -6,5 +6,13 @@
         /// Returns the given input of any type.
         /// </summary>
         public static T GetInput<T>(T input) => input;
+
+        /// <summary>
+        /// Closes your app with an exit code. The default exit code is 0.
+        /// </summary>
+        public static void CloseApp(int exitCode = 0)
+        {
+            Environment.Exit(exitCode);
+        }
     }
 }
