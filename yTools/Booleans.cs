c426c9ae8f44072dbd4233c4cab51dde9808308e
yesseruser <yesseruseryt@gmail.com>
2023-02-21 19:43:30 +0100

    Added the Booleans class.

---
+++ yTools/Booleans.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace yTools
+{
+    public static class Booleans
+    {
+        /// <summary>
+        /// The true boolean value.
+        /// </summary>
+        public const bool TRUE = true;
+
+        /// <summary>
+        /// The false boolean value.
+        /// </summary>
+        public const bool FALSE = false;
+
+        /// <summary>
+        /// Returns the negation of the input value.
+        /// </summary>
+        /// <param name="input">The input value to be negated</param>
+        /// <returns>The negated value.</returns>
+        public static bool Invert(bool input) => !input;
+    }
+}
