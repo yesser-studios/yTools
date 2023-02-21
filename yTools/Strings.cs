f8aecd8f81260472e0f5cd88b35c66f442416341
yesseruser <yesseruseryt@gmail.com>
2023-02-21 20:05:27 +0100

    Added the Strings class.

---
+++ yTools/Strings.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace yTools
+{
+    public static class Strings
+    {
+        /// <summary>
+        /// Writes the given string into the console with a new line at the end.
+        /// </summary>
+        public static void Write(string text)
+        {
+            Console.WriteLine(text);
+        }
+
+        /// <summary>
+        /// Writes the given string into the console without a new line at the end.
+        /// </summary>
+        public static void WriteWithoutEndLine(string text)
+        {
+            Console.Write(text);
+        }
+
+        /// <summary>
+        /// Writes the given string into the console colored with a new line at the end.
+        /// </summary>
+        public static void WriteColored(string text, ConsoleColor color)
+        {
+            ConsoleColor prevColor = Console.ForegroundColor;
+            Console.ForegroundColor = color;
+            Console.WriteLine(text);
+            Console.ForegroundColor = prevColor;
+        }
+
+        /// <summary>
+        /// Writes the given string into the console colored without a new line at the end.
+        /// </summary>
+        public static void WriteColoredWithoutEndLine(string text, ConsoleColor color)
+        {
+            ConsoleColor prevColor = Console.ForegroundColor;
+            Console.ForegroundColor = color;
+            Console.Write(text);
+            Console.ForegroundColor = prevColor;
+        }
+
+        /// <summary>
+        /// Joins an array of strings together into one string, without separating the characters.
+        /// </summary>
+        public static string JoinStrings(string[] strings)
+        {
+            string output = "";
+            foreach (string item in strings)
+            {
+                output += item;
+            }
+            return output;
+        }
+
+        /// <summary>
+        /// Joins an array of characters together into one string, without separating the characters.
+        /// </summary>
+        public static string JoinChars(char[] chars)
+        {
+            string output = "";
+            foreach (char item in chars)
+            {
+                output += item;
+            }
+            return output;
+        }
+
+        #region LowerUpper
+        /// <summary>
+        /// Converts the given string to lowercase.
+        /// </summary>
+        public static string Lower(string text) => text.ToLower();
+        /// <summary>
+        /// Converts the given string to uppercase.
+        /// </summary>
+        public static string Upper(string text) => text.ToUpper();
+        /// <summary>
+        /// Returns true if the given string is equal to null or whitespace.
+        /// </summary>
+        #endregion
+        public static bool IsNullOrWhitespace(string text) => string.IsNullOrWhiteSpace(text);
+    }
+}
