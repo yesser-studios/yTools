6e026ebebcbd9c0dc54ce44d94b7542d46e733ce
yesseruser <yesseruseryt@gmail.com>
2023-02-21 19:09:13 +0100

    Přidejte soubory projektu.

---
+++ yTools/General.cs
@@ -0,0 +1,10 @@
+﻿namespace yTools
+{
+    public static class General
+    {
+        /// <summary>
+        /// Returns the given input of any type.
+        /// </summary>
+        public static T GetInput<T>(T input) => input;
+    }
+}
