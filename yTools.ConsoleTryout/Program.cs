23d47bf3c41cde48f7edadebc009f8d1d5fa1335
yesseruser <yesseruseryt@gmail.com>
2023-04-20 20:00:24 +0200

    removed redundand usings and added a console project.

---
+++ yTools.ConsoleTryout/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace yTools.ConsoleTryout
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Doubles.maxDouble.ToString());
+        }
+    }
+}
