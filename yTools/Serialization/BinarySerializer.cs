aa1caf961d0c5962a607d238f9573d41cbd8dcc6
yesseruser <yesseruseryt@gmail.com>
2023-06-03 10:08:18 +0200

    Changed the naming of methods and classes.

--- yTools/Serialization/BinarySerializer.cs
+++ yTools/Serialization/BinarySerializer.cs
@@ -2,21 +2,21 @@
 using System.IO;
 using System.Runtime.Serialization.Formatters.Binary;
 
-namespace yTools
+namespace yTools.Serialization
 {
-    public class Cache
+    public class BinarySerializer
     {
         #region DefaultFolder
 
         string defaultFolder = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + @"\.cached";
 
         /// <summary>
-        /// Sets the default cache directory as a nested directory in <c>AppData\Local</c>.<br/>
+        /// Sets the default serialization directory to a nested directory in <c>AppData\Local</c>.<br/>
         /// This directory is <c>%LocalAppData%\.cached</c> by default.
         /// It is recommended to change it.
         /// </summary>
-        /// <param name="directory">The directory inside AppData\Local to store cache. Use '\' to nest directories.</param>
-        public void SetCacheDirectoryInLocalAppData(string directory)
+        /// <param name="directory">The directory inside AppData\Local to store serialized objects. Use '\' to nest directories.</param>
+        public void SetSerializationDirectoryInLocalAppData(string directory)
         {
             defaultFolder = directory[0] == '\\'
                 ? Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + directory
@@ -24,29 +24,29 @@ namespace yTools
         }
 
         /// <summary>
-        /// Sets the default cache directory.<br/>
+        /// Sets the default serialization directory.<br/>
         /// This directory is <c>%LocalAppData%\.cached</c> by default. It is recommended to change it.<br/>
-        /// It is recommended to use the Local AppData directory for caching objects; use <see cref="SetCacheDirectoryInLocalAppData(string)"/> instead.
+        /// It is recommended to use the Local AppData directory for caching objects; use <see cref="SetSerializationDirectoryInLocalAppData(string)"/> instead.
         /// </summary>
-        /// <param name="directory">The directory to store cache in. Enter full path including drive.</param>
-        public void SetCacheDirectory(string directory)
+        /// <param name="directory">The directory to store serialized objects in. Enter full path including drive.</param>
+        public void SetSerializationDirectory(string directory)
         {
             defaultFolder = directory;
         }
 
         #endregion
 
-        #region SaveCache
+        #region Serialize
 
         /// <summary>
-        /// Caches the given object of type T into the given file path.<br/>
+        /// Serializes the given object of type T into the given file path in a binary format.<br/>
         /// Make sure to include all directories, drive and filename in the path.<br/>
-        /// Returns true if the caching succeeded. Returns false if an exception was raised.
+        /// Returns true if the serialization succeeded. Returns false if an exception was raised.
         /// </summary>
         /// <param name="exception">The exception that occured while running this method. Null if no exception was raised.</param>
         /// <param name="exceptionType">The type of the exception. Null if no exception was raised.</param>
-        /// <returns>True if caching succeeded without exception.; false if an exception was raised.</returns>
-        public bool SaveCache<T>(string filepath, T obj, out Exception? exception, out Type? exceptionType)
+        /// <returns>True if serialization succeeded without exception.; false if an exception was raised.</returns>
+        public bool Serialize<T>(string filepath, T obj, out Exception? exception, out Type? exceptionType)
         {
             exception = null;
             exceptionType = null;
@@ -68,43 +68,43 @@ namespace yTools
         }
 
         /// <summary>
-        /// Caches the given object of type T into the given file name and parent directory.<br/>
+        /// Serializes the given object of type T into the given file name and parent directory.<br/>
         /// Make sure to include all directories and drive (full path) in the directory.<br/>
-        /// Returns true if the caching succeeded. Returns false if an exception was raised.
+        /// Returns true if the serialization succeeded. Returns false if an exception was raised.
         /// </summary>
         /// <param name="exception">The exception that occured while running this method. Null if no exception was raised.</param>
         /// <param name="exceptionType">The type of the exception. Null if no exception was raised.</param>
-        /// <returns>True if caching succeeded without exception.; false if an exception was raised.</returns>
-        public bool SaveCache<T>(T obj, string filename, string directory, out Exception? exception, out Type? exceptionType)
+        /// <returns>True if serialization succeeded without exception.; false if an exception was raised.</returns>
+        public bool Serialize<T>(T obj, string filename, string directory, out Exception? exception, out Type? exceptionType)
         {
-            return SaveCache(directory + @"\" + filename, obj, out exception, out exceptionType);
+            return Serialize(directory + @"\" + filename, obj, out exception, out exceptionType);
         }
 
         /// <summary>
-        /// Caches the given object of type T into the given file name inside the default parent directory set by <see cref="SetCacheDirectory(string)"/> or <see cref="SetCacheDirectoryInLocalAppData(string)"/>.<br/>
-        /// Returns true if the caching succeeded. Returns false if an exception was raised.
+        /// Serializes the given object of type T into the given file name inside the default parent directory set by <see cref="SetSerializationDirectory(string)"/> or <see cref="SetSerializationDirectoryInLocalAppData(string)"/>.<br/>
+        /// Returns true if the serialization succeeded. Returns false if an exception was raised.
         /// </summary>
         /// <param name="exception">The exception that occured while running this method. Null if no exception was raised.</param>
         /// <param name="exceptionType">The type of the exception. Null if no exception was raised.</param>
-        /// <returns>True if caching succeeded without exception.; false if an exception was raised.</returns>
-        public bool SaveCacheInDefault<T>(string filename, T obj, out Exception? exception, out Type? exceptionType)
+        /// <returns>True if serialization succeeded without exception.; false if an exception was raised.</returns>
+        public bool SerializeInDefault<T>(string filename, T obj, out Exception? exception, out Type? exceptionType)
         {
-            return SaveCache(obj, filename, defaultFolder, out exception, out exceptionType);
+            return Serialize(obj, filename, defaultFolder, out exception, out exceptionType);
         }
 
         #endregion
 
-        #region GetCache
+        #region Deserialize
 
         /// <summary>
-        /// Returns the cached object of type <see cref="object"/> from the given file path.<br/>
+        /// Returns the serialized object of type <see cref="object"/> from the given file path.<br/>
         /// Make sure to include all directories, drive and file name in the path.<br/>
         /// Returns true if the deserialization succeeded. Returns false if an exception was raised.
         /// </summary>
         /// <param name="exception">The exception that occured while running this method. Null if no exception was raised.</param>
         /// <param name="exceptionType">The type of the exception. Null if no exception was raised.</param>
-        /// <returns>True if decaching succeeded without exception; false if an exception was raised.</returns>
-        public object? GetCache<T>(string filepath, out Exception? exception, out Type? exceptionType)
+        /// <returns>True if deserialization succeeded without exception; false if an exception was raised.</returns>
+        public object? Deserialize<T>(string filepath, out Exception? exception, out Type? exceptionType)
         {
             exception = null;
             exceptionType = null;
@@ -126,29 +126,29 @@ namespace yTools
         }
 
         /// <summary>
-        /// Returns the cached object of type <see cref="object"/> from the given file name and directory.<br/>
+        /// Returns the serialized object of type <see cref="object"/> from the given file name and directory.<br/>
         /// Make sure to include all directories and drive in the directory path.<br/>
         /// Returns true if the deserialization succeeded. Returns false if an exception was raised.
         /// </summary>
         /// <param name="exception">The exception that occured while running this method. Null if no exception was raised.</param>
         /// <param name="exceptionType">The type of the exception. Null if no exception was raised.</param>
-        /// <returns>True if decaching succeeded without exception; false if an exception was raised.</returns>
-        public object? GetCache<T>(string filename, string directory, out Exception? exception, out Type? exceptionType)
+        /// <returns>True if deserialization succeeded without exception; false if an exception was raised.</returns>
+        public object? Deserialize<T>(string filename, string directory, out Exception? exception, out Type? exceptionType)
         {
-            return GetCache<T>(directory + @"\" + filename, out exception, out exceptionType);
+            return Deserialize<T>(directory + @"\" + filename, out exception, out exceptionType);
         }
 
         /// <summary>
-        /// Returns the cached object of type <see cref="object"/> from the given file name and default directory set by <see cref="SetCacheDirectory(string)"/> or <see cref="SetCacheDirectoryInLocalAppData(string)"/>.<br/>
+        /// Returns the serialized object of type <see cref="object"/> from the given file name and default directory set by <see cref="SetSerializationDirectory(string)"/> or <see cref="SetSerializationDirectoryInLocalAppData(string)"/>.<br/>
         /// Make sure to include all directories, drive and file name in the path.<br/>
         /// Returns true if the deserialization succeeded. Returns false if an exception was raised.
         /// </summary>
         /// <param name="exception">The exception that occured while running this method. Null if no exception was raised.</param>
         /// <param name="exceptionType">The type of the exception. Null if no exception was raised.</param>
-        /// <returns>True if decaching succeeded without exception; false if an exception was raised.</returns>
-        public object? GetCacheFromDefault<T>(string filename, out Exception? exception, out Type? exceptionType)
+        /// <returns>True if deserialization succeeded without exception; false if an exception was raised.</returns>
+        public object? DeserializeFromDefault<T>(string filename, out Exception? exception, out Type? exceptionType)
         {
-            return GetCache<T>(defaultFolder + @"\" + filename, out exception, out exceptionType);
+            return Deserialize<T>(defaultFolder + @"\" + filename, out exception, out exceptionType);
         }
 
         #endregion
