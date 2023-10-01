cb4d56d5dff86de8bce1e65c217732a4b640f425
yesseruser <yesseruseryt@gmail.com>
2023-10-01 17:33:58 +0200

    Split the tests into separate files.

---
+++ yTools.Tests/StringsTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace yTools.Tests
+{
+	[TestClass]
+	public class StringsTests
+	{
+        [TestMethod]
+        [DataRow(0)]
+        [DataRow(-526346.5)]
+        [DataRow(double.MaxValue)]
+        [DataRow(double.MinValue)]
+        public void ConvertToString(double value)
+        {
+            string? converted = Strings.ToString(value);
+            Assert.AreEqual(converted, value.ToString());
+        }
+    }
+}
+
