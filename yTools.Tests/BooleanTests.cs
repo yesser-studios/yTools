872b914ee242169b28b24a0e0d05203f20328741
yesseruser <yesseruseryt@gmail.com>
2024-02-10 12:06:20 +0100

    Improve test coverage
    Added test methods for Booleans, Integers and Strings.
    
    Signed-off-by: yesseruser <yesseruseryt@gmail.com>

---
+++ yTools.Tests/BooleanTests.cs
@@ -0,0 +1,13 @@
+﻿namespace yTools.Tests;
+
+[TestClass]
+public class BooleanTests
+{
+    [TestMethod]
+    [DataRow(true)]
+    [DataRow(false)]
+    public void Invert(bool boolean)
+    {
+        Assert.AreEqual(!boolean, Booleans.Invert(boolean));
+    }
+}
--- yTools.Tests/StringsTests.cs
+++ yTools.Tests/StringsTests.cs
@@ -23,12 +23,55 @@ namespace yTools.Tests
         public void JoinStrings(params string[] strings)
         {
             string joined = Strings.JoinStrings(strings);
+            
             string correct = "";
             foreach (var str in strings)
             {
                 correct += str;
             }
-            Assert.AreEqual(joined, correct);
+            
+            Assert.AreEqual(correct, joined);
+        }
+
+        [TestMethod]
+        [DataRow('h', 'e', 'l', 'l', 'o')]
+        [DataRow('h', 'r', 'u')]
+        [DataRow('i', 'd', 'k')]
+        public void JoinChars(params char[] chars)
+        {
+            string joined = Strings.JoinChars(chars);
+            
+            string correct = "";
+            foreach (var c in chars)
+            {
+                correct += c;
+            }
+            
+            Assert.AreEqual(correct, joined);
+        }
+
+        [TestMethod]
+        [DataRow("YO", "yo")]
+        [DataRow("hello", "hello")]
+        [DataRow("iDk", "idk")]
+        [DataRow("69", "69")]
+        [DataRow(".,'!", ".,'!")]
+        [DataRow("Mhm.", "mhm.")]
+        public void Lower(string str, string expected)
+        {
+            Assert.AreEqual(expected, Strings.Lower(str));
+        }
+        
+        [TestMethod]
+        [DataRow("YO", "YO")]
+        [DataRow("hello", "HELLO")]
+        [DataRow("iDk", "IDK")]
+        [DataRow("69", "69")]
+        [DataRow(".,'!", ".,'!")]
+        [DataRow("Mhm.", "MHM.")]
+        public void Upper(string str, string expected)
+        {
+            Assert.AreEqual(expected, Strings.Upper(str));
         }
     }
 }
