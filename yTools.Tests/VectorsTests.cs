ea9b305965a76aa8f8a15a68f4d68f6e43cecb1e
yesseruser <yesseruseryt@gmail.com>
2024-02-17 10:43:50 +0100

    Add tests for Vector2 and Vector3 casts

--- yTools.Tests/VectorsTests.cs
+++ yTools.Tests/VectorsTests.cs
@@ -26,4 +26,38 @@ public class VectorsTests
         
         Assert.AreEqual(expected, actual);
     }
+
+    [TestMethod]
+    public void Vector2Casts()
+    {
+        Vector2 vector = new(5, 6.5);
+        var castedSystemVector = (System.Numerics.Vector2)vector;
+
+        System.Numerics.Vector2 systemVector = new(4f, 8.5f);
+        Vector2 castedVector = systemVector;
+        
+        Assert.AreEqual(vector.X, castedSystemVector.X);
+        Assert.AreEqual(vector.Y, castedSystemVector.Y);
+        
+        Assert.AreEqual(systemVector.X, castedVector.X);
+        Assert.AreEqual(systemVector.Y, castedVector.Y);
+    }
+    
+    [TestMethod]
+    public void Vector3Casts()
+    {
+        Vector3 vector = new(5, 6.5, 8);
+        var castedSystemVector = (System.Numerics.Vector3)vector;
+
+        System.Numerics.Vector3 systemVector = new(4.1f, 8.5f, 45f);
+        Vector3 castedVector = systemVector;
+        
+        Assert.AreEqual(vector.X, castedSystemVector.X);
+        Assert.AreEqual(vector.Y, castedSystemVector.Y);
+        Assert.AreEqual(vector.Z, castedSystemVector.Z);
+        
+        Assert.AreEqual(systemVector.X, castedVector.X);
+        Assert.AreEqual(systemVector.Y, castedVector.Y);
+        Assert.AreEqual(systemVector.Z, castedVector.Z);
+    }
 }
