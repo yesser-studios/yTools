521df282d9f2f42714f12e4ac6213c71d3a7de22
yesseruser <yesseruseryt@gmail.com>
2024-09-28 12:36:45 +0200

    Add message to BinarySerializer obsoletion

--- yTools/Serialization/BinarySerializer.cs
+++ yTools/Serialization/BinarySerializer.cs
@@ -4,7 +4,7 @@ using System.Runtime.Serialization.Formatters.Binary;
 
 namespace yTools.Serialization
 {
-    [Obsolete]
+    [Obsolete("Binary serialization is not trustworthy. Use JsonSerializer or XmlSerializer instead.")]
     public class BinarySerializer
     {
         #region DefaultFolder
