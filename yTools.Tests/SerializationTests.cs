cdd15375350757b064241a50b0bde2de37707486
yesseruser <yesseruseryt@gmail.com>
2025-10-13 00:26:35 +0200

    Cleanup proper folder

--- yTools.Tests/SerializationTests.cs
+++ yTools.Tests/SerializationTests.cs
@@ -4,17 +4,12 @@
 	public class SerializationTests
     {
         private readonly char _sep = General.PathSeparator;
-        
+
         [TestCleanup]
         public void Cleanup()
         {
-            const string testDirectory = "yTools";
-
-            var fullDir = $"{Environment.GetFolderPath(
-                Environment.SpecialFolder.LocalApplicationData
-            )}{_sep}{testDirectory}";
-            if (Directory.Exists(fullDir))
-                Directory.Delete(fullDir, true);
+            if (Directory.Exists("./test-serialization"))
+                Directory.Delete("./test-serialization", true);
         }
 
         [TestMethod]
